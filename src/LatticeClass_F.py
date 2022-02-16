a8bd79314e5e671adb80eafd11728f04ded3dd0a
Ramenspazz <kirisame.ramen@gmail.com>
2022-02-16 15:44:13 -0800

    Update LatticeClass_F.py
    plt.clf() causes multiple windows to be drawn at once for some strange reason.

--- src/LatticeClass_F.py
+++ src/LatticeClass_F.py
@@ -234,7 +234,6 @@ class lattice_class:
             plt.savefig(fname)
         if auto_plot is True:
             plt.show()
-        plt.clf()
 
     def plot_metrop(self, SE_mtx: ndarray, BJ: list | ndarray,
                     times: Optional[int] = None,
@@ -290,7 +289,6 @@ class lattice_class:
             plt.savefig(fname)
         if auto_plot is True:
             plt.show()
-        plt.clf()
 
 # TODO: Look into this http://mcwa.csi.cuny.edu/umass/izing/Ising_text.pdf
 # TODO: the worm algorithm.
