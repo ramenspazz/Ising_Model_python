1ec0679c911cb97ec31faf8902df3233c2151cca
Ramenspazz <kirisame.ramen@gmail.com>
2022-03-01 23:14:50 -0800

    Update main.py

--- Project_files/main.py
+++ Project_files/main.py
@@ -50,14 +50,14 @@ def generate_random(gen_num: int) -> list:
 
 def main(*args, **kwargs) -> int:
     try:
-        N = 12
-        M = 12
+        N = 256
+        M = 256
         size = [N, M]
-        total_time = 100  # 4*math.trunc(np.sqrt(N*M))
+        total_time = 1000  # 4*math.trunc(np.sqrt(N*M))
         print(total_time)
         a = 0.1
         b = 10+a
-        num_points = 100
+        num_points = 10
         step = (b-a)/num_points
         BJs = np.arange(a, b, step)
         output = ''
